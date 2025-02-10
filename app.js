dbccbf19503ca76c8e7636edbd12189934d63e65
Carlos <carlosmarinvaldivia@gmail.com>
2025-02-10 18:29:42 -0300

    Actualización 1 del app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let amigos = [];
+
+function agregarAmigo () {
+    let nuevoAmigo = document.getElementById("amigo").value;
+
+    if (nuevoAmigo.trim() !== "") {
+        amigos.push(nuevoAmigo);
+        document.getElementById("amigo").value = "";
+        actualizarListaAmigos();
+  } else {
+    alert("Por favor, ingresa un nombre válido.");
+  }
+}
