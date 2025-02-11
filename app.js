0b3eaaa575af36679dc353eea487b927d4d43574
Carlos <carlosmarinvaldivia@gmail.com>
2025-02-11 17:49:58 -0300

    Actualizado final  app.js
    Luego de haber realizado el desarrollo de la programación en Jaba Script de manera directa en el editor VSC y de haber realizado las pruebas correspondientes a través del Live Server, verificando que el programa funcionara correctamente, se procedió a ingresar los datos en la ventana de VSC online de GitHub.

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+/** Variable array que almacenará los datos de los nombres ingresados.
+ */
+
 let amigos = [];
 
-function agregarAmigo () {
-    let nuevoAmigo = document.getElementById("amigo").value;
+/**
+* Ingresar un nuevo amigo al array.
+* Validar la entrada y verificar que el campo de entrada no esté vacío.
+* Actualizar el array de amigos.
+*/
+function agregarAmigo() {
+  let nuevoAmigo = document.getElementById("amigo").value; 
+  if (nuevoAmigo.trim() !== "") {
+    amigos.push(nuevoAmigo);
 
-    if (nuevoAmigo.trim() !== "") {
-        amigos.push(nuevoAmigo);
-        document.getElementById("amigo").value = "";
-        actualizarListaAmigos();
+    // Limpiar la lista existente.
+    document.getElementById("amigo").value = "";
+
+    actualizarListaAmigos();
   } else {
     alert("Por favor, ingresa un nombre válido.");
   }
 }
+
+/**
+* Actualizar lista de amigos y crear elementos <li> para cada amigo.
+*/
+function actualizarListaAmigos() {
+   let listaAmigosElement = document.getElementById("listaAmigos");
+    listaAmigosElement.innerHTML = "";
+  
+    for (let i = 0; i < amigos.length; i++) {
+      let nuevoElemento = document.createElement("li");
+      nuevoElemento.textContent = amigos[i];
+      listaAmigosElement.appendChild(nuevoElemento);
+    }
+  }
+
+/**
+* Sortear y seleccionar de manera aleatoria uno de los nombres almacenados en el array amigos.
+* Validar que haya amigos disponibles previo a la realización del sorteo.
+* Obtener el nombre sorteado.
+* Mostrar el resultado.
+*/
+  function sortearAmigo() {
+    let cantidadAmigos = amigos.length;
+
+    if (cantidadAmigos === 0) {
+      alert("Por favor, inserte un nombre antes de sortear");
+    } else {
+      let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+      let resultadoHTML = document.querySelector("#resultado");
+      resultadoHTML.innerHTML = amigos[indiceAmigo];
+    }
+  }
